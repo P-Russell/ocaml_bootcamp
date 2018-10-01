310f9cc2137c6937fd936b0c79394c69fe3268c5
P-Russell <pdrussell011@gmail.com>
2018-10-01 16:00:29 +0200

    exo8

--- ex08/ft_rot_n.ml
+++ ex08/ft_rot_n.ml
@@ -6,7 +6,7 @@
 (*   By: prussell <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2018/10/01 15:31:29 by prussell          #+#    #+#             *)
-(*   Updated: 2018/10/01 15:52:30 by prussell         ###   ########.fr       *)
+(*   Updated: 2018/10/01 15:58:12 by prussell         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -16,17 +16,18 @@ let is_upper (c : char) =
 let is_lower (c : char) = 
     c >= 'a' && c <= 'z'
 
-let rotate_upper (n : int)(c : char) = 
-    if (n + int_of_char c) < (int_of_char 'Z') then
-        char_of_int (n + int_of_char c)
+let rotate_upper (c : char) = 
+    if (1 + int_of_char c) < (int_of_char 'Z') then
+        char_of_int (1 + int_of_char c)
     else
-        char_of_int ((n + int_of_char c) + (int_of_char 'A'))
+        char_of_int ((1 + int_of_char c) mod int_of_char 'Z' + (int_of_char 'A'))
 
 let ft_rot_n n str = 
-    let 
+    String.map rotate_upper str
 
 
 let main() =
-    ft_rot_n 1 "ABCDEF"
+    let x = ft_rot_n 1 "ABCDEFGHIJKLMNOPQRSTUVWXYZ" in
+        print_endline x
 
 let() = main()
